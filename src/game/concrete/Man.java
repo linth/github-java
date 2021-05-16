66bd754c6032a93740860e4acb1579913375a7ce
linth <zhua0404@gmail.com>
2021-05-16 19:33:50 +0800

    add overload example code.

--- src/game/concrete/Man.java
+++ src/game/concrete/Man.java
@@ -1,5 +1,10 @@
 package game.concrete;
 
+/**
+ * Reference:
+ *  - https://www.liaoxuefeng.com/wiki/1252599548343744/1260454256258656
+ */
+
 import game.abs.Person;
 
 public class Man extends Person {
@@ -14,5 +19,19 @@ public class Man extends Person {
 
     public void showHello() {
         System.out.println("This is Man showHello()...");
+    }
+
+    public void show() {
+        System.out.println("Do not any input argument for show()...");
+    }
+
+    public void show(String s) {
+        // Overload.
+        System.out.println("Have input argument: (string) " + s + " for show()...");
+    }
+    
+    public void show(int a) {
+        // Overload.
+        System.out.println("Have input argument: (int) " + a + " for show()...");
     }
 }
