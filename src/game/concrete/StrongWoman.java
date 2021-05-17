2383af7fc95f267ad5869194da2224dec738665d
linth <zhua0404@gmail.com>
2021-05-17 11:29:32 +0800

    add strongwoman class.

---
+++ src/game/concrete/StrongWoman.java
@@ -0,0 +1,9 @@
+package game.concrete;
+
+public class StrongWoman extends Woman {
+    
+
+    public void showHello() {
+        System.out.println("This is StrongWoman showHello()...");
+    }
+}
