6ca805983c2cac945758dd6c133cb7af07b5da51
linth <zhua0404@gmail.com>
2021-06-17 22:52:42 +0800

    add CustomExceptionExample.

---
+++ src/example/ExceptionExample/CustomExceptionExample.java
@@ -0,0 +1,28 @@
+package example.ExceptionExample;
+
+class DefinedCustomException extends Exception {
+    String s;
+
+    DefinedCustomException(String msg) {
+        s = msg;
+    }
+
+    public String toString() {
+        return ("My defined exception. " + s);
+    }
+}
+
+public class CustomExceptionExample {
+    public static void main(String[] args) {
+        try {
+            System.out.println("go into the try block...");
+            throw new DefinedCustomException("This is my defined exception.");
+        } catch (DefinedCustomException e) {
+            System.out.println("go into the catch block...");
+            System.out.println(e + "; Error message: " + e.s);
+        } catch (Exception e) {
+            System.out.println("this is exception block...");
+            System.out.println(e);
+        }
+    }
+}
