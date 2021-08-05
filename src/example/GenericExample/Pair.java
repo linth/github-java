94028a4f0726b9ec2b7b980e6cca17ed9b6acf3e
linth <zhua0404@gmail.com>
2021-08-06 00:17:02 +0800

    add comments for pair generic.

--- src/example/GenericExample/Pair.java
+++ src/example/GenericExample/Pair.java
@@ -1,5 +1,13 @@
 package example.GenericExample;
 
+/**
+ * 泛型<T> 不能用在靜態方法 (static method), 除非使用 static <T> Pair<T>才可以。
+ * 
+ * Reference:
+ *  - https://www.liaoxuefeng.com/wiki/1252599548343744/1265105853480864
+ * 
+ * @param <T>
+ */
 public class Pair<T> {
     // same type for parameters.
     private T fist;
@@ -19,6 +27,7 @@ public class Pair<T> {
     }
 
     public static <K> Pair<K> create(K first, K last) {
+        //! 注意如果使用 public static Pair<T> create() 則會有錯誤訊息，需要使用 <T> Pair<T> create().s
         return new Pair<K>(first, last);
     }
 
