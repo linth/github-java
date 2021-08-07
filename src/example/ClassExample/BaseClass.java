0d0fc185d106a466d573d816faf59e6ccaa8ed74
linth <zhua0404@gmail.com>
2021-08-07 14:12:07 +0800

    add function and comments.

--- src/example/ClassExample/BaseClass.java
+++ src/example/ClassExample/BaseClass.java
@@ -1,18 +1,24 @@
 package example.ClassExample;
 
+/**
+ * 三層架構：interface class, abstract class, concrete class.
+ */
 interface OriginInterface {
+    // interface class.
     public void printSelf();
 
     public void printHello();
 }
 
 abstract class Origin {
+    // abtstact class.
     public void printHello() {
         System.out.println("Hello");
     }
 }
 
 class AA extends Origin {
+    // concorete class.
     public void printSelf() {
         System.out.println("AA");
     }
@@ -23,8 +29,26 @@ class AA extends Origin {
 }
 
 class BB extends AA {
+    // concorete class.
+    @Override
     public void printSelf() {
         System.out.println("BB");
+    }
+}
+
+/**
+ * 覆寫(Override)是指子類別繼承父類別時，改寫父類別原有的方法內容
+ */
+class CC extends AA {
+    // concorete class.
+    @Override
+    public void printSelf() {
+        System.out.println("I re-define a new print self function.");
+    }
+
+    @Override
+    public void printHello() {
+        System.out.println("I re-define a new print hello function.");
     }
 }
 
@@ -34,8 +58,14 @@ public class BaseClass {
         // b.printA();
         AA a = new AA();
         a.printSelf();
+        a.printHello();
 
         BB b = new BB();
         b.printSelf();
+        b.printHello();
+
+        CC c = new CC();
+        c.printSelf();
+        c.printHello();
     }
 }
