db34da9c35cd49bbe5f91b3e00e8a7d0e5c0afc3
linth <zhua0404@gmail.com>
2021-08-31 23:08:50 +0800

    add comments.

--- src/example/SOLIDExample/LSPExample.java
+++ src/example/SOLIDExample/LSPExample.java
@@ -1,10 +1,17 @@
 package example.SOLIDExample;
 
 /**
- * Liskov substitution principle
+ * 里氏替換原則 (Liskov substitution principle)
+ *  - 子型別要完全實作父型別的方法
+ *  - 子型別要能完全替代父型別，且不能出現錯誤或異常
+ * 
+ * 里氏替換原則優點
+ *  - 增加程式碼的健全度，在使用不同的子類別的時候，可以大幅度的保證彼此之間的相容性。只要父類別可以使用，基本上子類別也可以使用
+ *  - 子類別如果要新增功能，獨立在父類別的功能之外，才不會在搬移到其他子類別的時候發生奇怪的問題，也可以將功能切分乾淨，區分職責
  * 
  * Reference:
  *  - https://www.edureka.co/blog/solid-principles-in-java/
+ *  - https://ithelp.ithome.com.tw/articles/10248355
  */
 public class LSPExample {
     public static void main(String[] args) {
@@ -15,6 +22,11 @@ public class LSPExample {
     }
 }
 
+/**
+ * 建立RectangleV2 class，以及建立 Square 並繼承 RectangleV2
+ * 
+ * 也可以參考 OCP example.
+ */
 class RectangleV2 {
     private int length;
     private int breadth;
