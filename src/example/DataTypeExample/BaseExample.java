7f80d4d69c8592ec01cb5be5898328fef7cbb4fa
linth <zhua0404@gmail.com>
2021-09-02 22:27:10 +0800

    add simple data type.

---
+++ src/example/DataTypeExample/BaseExample.java
@@ -0,0 +1,20 @@
+package example.DataTypeExample;
+
+public class BaseExample {
+    private float height = 178.3f; // 32位元 （-3.4e+38 ~ 3.4e+38）
+    private float weight = 77.12F;
+    
+    private double numberDouble = 5.222222222d; // 64位元 （-1.79e+308 ~ 1.79e+308）
+    private double avergeNumber = 2.55;
+
+    private int math = 99;
+    private char engWord = 'a';
+
+    private boolean isBoy = false;
+
+    private String description = "Hi, my name is George.";
+
+    private final double PI = 3.14159; // 不能改變內容
+}
+
+
