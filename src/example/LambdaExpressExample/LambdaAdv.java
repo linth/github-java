b5fe2c74cc0d60fe6a3ecd2a1bb3926b9b6c32c5
linth <zhua0404@gmail.com>
2021-09-07 00:30:38 +0800

    add two empty examples for Lambda expression.

---
+++ src/example/LambdaExpressExample/LambdaAdv.java
@@ -0,0 +1,11 @@
+package example.LambdaExpressExample;
+
+/**
+ * Lambda 進階表達式
+ * 
+ * Reference 
+ *  - https://mrbird.cc/java8lambda2.html
+ */
+public class LambdaAdv {
+    
+}
---
+++ src/example/LambdaExpressExample/LambdaBase.java
@@ -0,0 +1,11 @@
+package example.LambdaExpressExample;
+
+/**
+ * Lambda 表達式
+ * 
+ * Reference
+ *  - https://mrbird.cc/java8lambda1.html
+ */
+public class LambdaBase {
+    
+}
