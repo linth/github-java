b49e7d2f8189adacb20ede99ff4703237db2befd
linth <zhua0404@gmail.com>
2021-09-06 23:43:17 +0800

    add distinct function for stream.

--- src/example/StreamExample/StreamBase.java
+++ src/example/StreamExample/StreamBase.java
@@ -11,9 +11,14 @@ import java.util.List;
 public class StreamBase {
     public static void main(String[] args) {
         List<String> list = Arrays.asList("Java", "JavaScript", "python", "PHP", "C#", "Golang", "Swift");
+        List<Integer> num = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
 
         findJChangeBig(list);
         dealWithStream(list);
+
+        StreamTask streamTask = new StreamTask();
+        streamTask.filterShapAndShow(list);
+        streamTask.distinctEven(num);
     }
 
     // 傳統作法使用for-loop一個一個處理
@@ -37,5 +42,20 @@ public class StreamBase {
             .filter(s -> s.startsWith("J"))
             .map(String::toUpperCase)
             .forEach(System.out::println);
+    }
+}
+
+class StreamTask {
+    public void filterShapAndShow(List<String> list) {
+        list.stream()
+            .filter(s -> s.contains("#"))
+            .forEach(System.out::println);
+    }
+
+    public void distinctEven(List<Integer> num) {
+        num.stream()
+            .filter(i -> i % 2 == 0)
+            .distinct()
+            .forEach(i -> System.out.println(i));
     }
 }
