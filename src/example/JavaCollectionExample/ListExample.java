082b1f2960d9af83fb4f24a72600793e5164c9c2
linth <zhua0404@gmail.com>
2021-10-10 11:41:11 +0800

    add list example and comments.

--- src/example/JavaCollectionExample/ListExample.java
+++ src/example/JavaCollectionExample/ListExample.java
@@ -3,6 +3,19 @@ package example.JavaCollectionExample;
 import java.util.*;
 
 /**
+ * List 範例
+ * 
+ *             ----------
+ *            |  List    |  
+ *             ----------    
+ *             ＾       ＾    
+ *  implements |        | implements
+ *            /          \    
+ *          /             \
+ *  ----------          -----------  
+ * |ArrayList |        |LinkedList |  
+ *  ----------          -----------   
+ * 
  * Reference:
  *  - https://beginnersbook.com/2013/12/java-arraylist/
  */
@@ -23,6 +36,11 @@ public class ListExample {
         while (itr.hasNext()) {
             System.out.println(itr.next());
         }
+
+        // FirstList example.
+        FirstList firstList = new FirstList();
+        firstList.createListArray();
+        firstList.createList();
     }
 
     public void forEachArrayList(ArrayList<String> list) {
@@ -52,5 +70,38 @@ public class ListExample {
 
     public int getNumberOfElement(ArrayList<String> list) {
         return list.size();
+    }
+}
+
+class FirstList {
+    private List<?> list;
+
+    public void createListArray() {
+        ArrayList<String> list = new ArrayList<String>();
+        addData(list)
+            .showDataByForEach(list, "ArrayList");
+    }
+    //! 請注意spring寫法通常使用 interface class 方式，統一對接口，實體化則各自呼叫。
+    public void createList() {
+        List<String> list = new ArrayList<String>();
+        addData(list)
+            .showDataByForEach(list, "list");
+    }
+
+    public FirstList addData(List<String> list) {
+        list.add("TW");
+        list.add("US");
+        list.add("HK");
+        this.list = list;
+        return this;
+    }
+
+    public void showDataByForEach(List<?> list, String listName) {
+        System.out.println("Class Type: " + list.getClass() + " " + System.identityHashCode(list));
+        System.out.println("開始執行" + listName + "...");
+        this.list.forEach(s -> {
+            System.out.println(s);
+        });
+        System.out.println("結束" + listName + "...\n");
     }
 }
