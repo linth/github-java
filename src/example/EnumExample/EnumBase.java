0a3a93f93ea1ac135b43eb859f190fcbc4c8b24d
linth <zhua0404@gmail.com>
2021-10-11 23:30:26 +0800

    add customEnum, class example, and comments.

--- src/example/EnumExample/EnumBase.java
+++ src/example/EnumExample/EnumBase.java
@@ -3,6 +3,7 @@ package example.EnumExample;
 /**
  * Reference
  *  - https://beginnersbook.com/2014/09/java-enum-examples/
+ *  - https://github.com/linth/JavaGuide/blob/master/docs/java/basic/%E7%94%A8%E5%A5%BDJava%E4%B8%AD%E7%9A%84%E6%9E%9A%E4%B8%BE%E7%9C%9F%E7%9A%84%E6%B2%A1%E6%9C%89%E9%82%A3%E4%B9%88%E7%AE%80%E5%8D%95.md
  */
 enum Directions {
     EAST ("E"),
