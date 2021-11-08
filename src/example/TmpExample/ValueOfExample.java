efaac50ea5056c72a9f1418ea54aa8c7c660ab22
linth <zhua0404@gmail.com>
2021-11-08 16:11:14 +0800

    add valueOf example.

---
+++ src/example/TmpExample/ValueOfExample.java
@@ -0,0 +1,21 @@
+package example.TmpExample;
+
+/**
+ * ValueOf() 方法
+ * 
+ * Reference:
+ *  - https://www.runoob.com/java/java-string-valueof.html
+ */
+public class ValueOfExample {
+    public static void main(String[] args) {
+        int2String();
+    }
+
+    public static void int2String() {
+        Integer val = 3;
+        String valString = String.valueOf(val);
+        
+        System.out.println("val: " + val + "; type: " + val.getClass());
+        System.out.println("conver to string val: " + valString + "; type: " + valString.getClass());
+    }
+}
