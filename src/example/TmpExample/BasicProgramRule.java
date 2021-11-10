51c0d79966695d3575dff2df360f28509b6aed14
linth <zhua0404@gmail.com>
2021-11-10 15:40:47 +0800

    add basic program rule.

---
+++ src/example/TmpExample/BasicProgramRule.java
@@ -0,0 +1,35 @@
+package example.TmpExample;
+
+/**
+ * Java 最常見基礎知識總結
+ * 
+ * 一．基本資料型別
+ * 
+ * 二．陣列
+ * 
+ * 三．基本語法
+ * 
+ * 四．控制結構
+ *  - If：if··else 只有一句需要執行的語句時可以省略｛｝ 等號兩邊不用空格
+ *  - For：形式：for（表示式1；表示式2；表示式3；）｛語句；｝執行時判斷是否結束的是表示式2，如果表示式2是false輸出結果。
+ *  - While/do··while：while：先判斷在執行，do···while：先執行在判斷。（無論是否滿足表示式，都先執行一遍）
+ *  - Switch：條件補充語句：格式case xx； case xx； default ；在需要輸出case值時用break語句，多個case可以合併到一起。
+ *  - Break/continue：break語句終止某個語句塊的執行，在迴圈語句中可以強行退出迴圈 continue 終止某一次迴圈，跳過這次迴圈，開始下一次迴圈過程。
+ * 
+ * 五．Java虛擬機器的概念，JDK的概念
+ *  - Java虛擬機器是抽象的計算機，是java語言的執行環境
+ *  - Java虛擬機器包括一套位元組碼指令集、一組暫存器、一個棧、一個垃圾回收堆和一個儲存方法域、還有相應的指令系統。
+ *  - JDK概念：java開發工具。
+ * 
+ * 六．類的封裝性
+ * 
+ * 七．方法的呼叫，靜態方法和例項方法
+ * 
+ * 八．面對物件程式設計的三個表現
+ * 
+ * Reference:
+ *  - https://www.gushiciku.cn/pl/aawk/zh-tw
+ */
+public class BasicProgramRule {
+    
+}
