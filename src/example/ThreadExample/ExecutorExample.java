8d38ab7ccdf768e78e495555d3980af7b476089b
linth <zhua0404@gmail.com>
2021-11-10 15:39:18 +0800

    add an empty Executor Example.

---
+++ src/example/ThreadExample/ExecutorExample.java
@@ -0,0 +1,16 @@
+package example.ThreadExample;
+
+/**
+ * Java Executor & ThreadPoolExecutor 基本觀念
+ * 
+ * 《阿里巴巴 Java 開發手冊》中強制執行緒池不允許使用 Executors 去建立，而是通過 ThreadPoolExecutor 建構函式的方式，這樣的處理方式讓寫的同學更加明確執行緒池的執行規則，規避資源耗盡的風險。
+ * 
+ * Reference:
+ *  - https://yu-jack.github.io/2019/02/19/java-executor/
+ *  - https://www.gushiciku.cn/pl/alQi/zh-tw
+ */
+public class ExecutorExample {
+    public static void main(String[] args) {
+        
+    }
+}
