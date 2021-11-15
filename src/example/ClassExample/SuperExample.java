2964fbb3d145b31b51ee78daa5b18aa31983cdd7
linth <zhua0404@gmail.com>
2021-11-15 21:53:47 +0800

    modify super example.

--- src/example/ClassExample/SuperExample.java
+++ src/example/ClassExample/SuperExample.java
@@ -4,30 +4,63 @@ package example.ClassExample;
  * 使用super()範例
  * 
  * Animal class 已經被定義在 InheritanceInterface 裡面。
+ * 
+ * Reference:
+ *  - 
  */
 public class SuperExample {
     public static void main(String[] args) {
-        Humanity humanity = new Humanity("Peter");
-        System.out.println(humanity.getName());
+        // parent class.
+        Ape ape = new Ape("George", 18);
+        System.out.println("name: " + ape.getName() + "; age: " + ape.getAge());
+
+        // child class.
+        Humanity humanity = new Humanity("Peter", 77, 180, 56.1);
+        System.out.println("name: " + humanity.getName() + "; age: " + humanity.getAge() + "; height: " + humanity.getHeight() + "; weight: " + humanity.getWeight());
     }
 }
 
 class Ape implements Animal {
     protected String name;
+    protected int age;
 
-    Ape(String name) {
+    Ape(String name, int age) {
         this.name = name;
+        this.age = age;
+        System.out.println("create Ape constructor.");
     }
 
     public String getName() {
-        return this.name;
+        return name;
+    }
+
+    public int getAge() {
+        return age;
     }
 }
 
 class Humanity extends Ape {
+    private String studentId;
+    private double height;
+    private double weight;
 
-    Humanity(String name) {
-        super(name);
+    Humanity(String name, int age, double height, double weight) {
+        super(name, age);
+        this.height = height;
+        this.weight = weight;
+    }
+
+    Humanity(String name, int age) {
+        super(name, age);
+        System.out.println("create Humanity constructor.");
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWeight() {
+        return weight;
     }
 }
 
