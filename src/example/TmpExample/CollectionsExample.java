1ed5449fc0625f304eab771d93e52f9366ea2c23
linth <zhua0404@gmail.com>
2021-11-15 17:53:47 +0800

    add an empty collection example.

---
+++ src/example/TmpExample/CollectionsExample.java
@@ -0,0 +1,22 @@
+package example.TmpExample;
+
+/**
+ * Java Collections 觀念跟範例
+ * 
+ * 優點:
+ *  - 不變的API，不同Class可以共用相同的API(例如 add(), remove(), contains(), isEmpty(), addAll() 等等)。
+ *  - 減少開發時間，不用重新設計新的collection。
+ *  - 增加程式的速度及質素，由於Collection提供了可靠的數據結構及算法在不同interface。
+ * 
+ * Collection interface之下有三個 Interface:
+ *  - Set (LinkedHashSet, HashSet, TreeSet)
+ *  - List (ArrayList, LinkedList, Stack)
+ *  - Queue (LinkedList, ArrayBlockingQueue, PriorityQueue)
+ * 
+ * Reference:
+ *  - https://ithelp.ithome.com.tw/articles/10229641?sc=pt
+ *  - https://ithelp.ithome.com.tw/articles/10229634
+ */
+public class CollectionsExample {
+    
+}
