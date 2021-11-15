a10df73c99f34e401832ebfa98e2b1d98d0a0bdb
linth <zhua0404@gmail.com>
2021-11-15 17:53:14 +0800

    add comment.

--- src/example/GenericExample/GenericCollectionExample.java
+++ src/example/GenericExample/GenericCollectionExample.java
@@ -6,6 +6,7 @@ package example.GenericExample;
  * Reference:
  *  - http://mahaljsp.asuscomm.com/index.php/2016/10/28/java_collections/
  *  - https://iter01.com/597570.html
+ *  - https://ithelp.ithome.com.tw/articles/10275201
  * 
  */
 public class GenericCollectionExample {
