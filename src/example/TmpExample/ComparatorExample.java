421db2b2948e8c81874d1a9f17b61fbd11c8730c
linth <zhua0404@gmail.com>
2021-11-15 15:37:48 +0800

    add an empty comparator example.

---
+++ src/example/TmpExample/ComparatorExample.java
@@ -0,0 +1,13 @@
+package example.TmpExample;
+
+/**
+ * Comparator & Comparable 範例
+ * 
+ * Reference:
+ *  - https://dotblogs.com.tw/duke/2019/12/13/151221
+ */
+public class ComparatorExample {
+    public static void main(String[] args) {
+        
+    }
+}
