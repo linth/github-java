44135190ff463d45b00b2fec43e5f21e82f9ffe2
linth <zhua0404@gmail.com>
2021-11-17 18:11:39 +0800

    add advanced stream handling example.

---
+++ src/example/StreamExample/AdvancedTwoStreamExample.java
@@ -0,0 +1,87 @@
+package example.StreamExample;
+
+import java.security.PublicKey;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+/**
+ * 兩個以上的 stream 進行交集、聯集、差集範例
+ * 
+ * Reference:
+ *  - https://www.gushiciku.cn/pl/pjFy/zh-tw
+ */
+public class AdvancedTwoStreamExample {
+    public static void main(String[] args) {
+        // interaction example.
+        interactionExample();
+
+        // 
+    }
+
+    public static void interactionExample() {
+        // * interaction example.
+        List<String> list1 = data();
+        List<String> list2 = data2();
+
+        List<String> result = interaction(list1, list2);
+
+        Optional<List<String>> finalResult = Optional.ofNullable(result);
+        // List<String> a = new ArrayList<>();
+        // finalResult.orElse(a); 
+        System.out.println(finalResult);
+    }
+
+    // TODO: finish union example.
+    // public static void unionExample() {
+    //     // * union example.
+    //     List<String> list1 = data();
+    //     List<String> list2 = data2();
+        
+    //     List<String> result = union(list1, list2);
+    // }
+
+    // TODO: finish subtraction example.
+    // public static void subtractionExample() {
+    //     // * union example.
+    //     List<String> list1 = data();
+    //     List<String> list2 = data2();
+        
+    //     List<String> result = subtraction(list1, list2);
+    // }
+
+    public static List<String> data() {
+        // * generate a list of data.
+        List<String> list = Arrays.asList("Java", "JavaScript", "python", "PHP", "C#");
+        return list;
+    }
+
+    public static List<String> data2() {
+        // * generate a list of data2.
+        List<String> list = Arrays.asList("Java", "JavaScript", "C#", "Golang", "Swift");
+        return list;
+    }
+
+    public static List<String> interaction(List<String> firstList, List<String> secondList) {
+        // * find the interaction of two Lists.
+        // List<String> result = new ArrayList<>();
+        return firstList.stream()
+            .filter(s -> secondList.contains(s))
+            .collect(Collectors.toList());
+    }
+
+    public static List<String> union(List<String> firstList, List<String> secondList) {
+        // * find the union of two Lists.
+        List<String> list = new ArrayList<>();
+        return list;
+    }
+
+    public static List<String> subtraction(List<String> firstList, List<String> secondList) {
+        // * find the subtraction of two Lists.
+        List<String> list = new ArrayList<>();
+        return list;
+    }
+}
