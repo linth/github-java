75159f8b88b645e67b1791d63b2652543003f43f
linth <zhua0404@gmail.com>
2021-11-17 11:34:24 +0800

    update basic thread example.

--- src/example/ThreadExample/BasicThread.java
+++ src/example/ThreadExample/BasicThread.java
@@ -4,13 +4,6 @@ package example.ThreadExample;
  * Thread basic concept.
  */
 public class BasicThread {
-    public void basic_example() {
-        Thread thread = Thread.currentThread();
-        System.out.println("The thread name: " + thread.getName());
-        thread.setName("TheMainThread");
-        System.out.println("The thread name: " + thread.getName());
-        System.out.println("The thread ID: " + thread.getId());
-    }
     public static void main(String[] args) {
         // example for get thread name and id.
         BasicThread bt = new BasicThread();
@@ -20,7 +13,36 @@ public class BasicThread {
         RunThreadExample rhe = new RunThreadExample("FirstOneThread");
         rhe.start();
         System.out.println("the default thread name: " + rhe.getName());
+
+        bt.createNThread(3);
     }
+
+    public void basic_example() {
+        // TODO: make sure that the currentThread().
+        Thread thread = Thread.currentThread();
+        System.out.println("Thread 名稱: " + thread.getName());
+        thread.setName("TheMainThread"); // 可以改 thread 名稱
+        System.out.println("The thread name: " + thread.getName());
+        System.out.println("The thread ID: " + thread.getId());
+    }
+
+    public void createNThread(int n) {
+        // generate n threads.
+        try {
+            if (n > 0) {
+                for (int i=0; i<n; i++) {
+                    RunThreadExample rhe = new RunThreadExample("Thread " + i);
+                    System.out.println("Thread number " + i + ": " + rhe.getName() + "; id: " + rhe.getId());
+                    rhe.run();
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("error: " + e.toString());
+        }
+    }
+
+    // TODO: what's next?
+    // * create n threads, and then run different programs with different threads.
 }
 
 /**
@@ -32,6 +54,6 @@ class RunThreadExample extends Thread {
     }
 
     public void run() {
-        System.out.println("Thread is running...");
+        System.out.println("Thread is running..." + " (thread name: " + this.getName() + "; thread id: " + this.getId() + ")");
     }
 }
