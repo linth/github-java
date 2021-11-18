8dc1682be04313fba04229e3796dfa2003323ac6
linth <zhua0404@gmail.com>
2021-11-18 18:01:05 +0800

    add hashmap example.

--- src/example/JavaCollectionExample/MapExample.java
+++ src/example/JavaCollectionExample/MapExample.java
@@ -1,6 +1,7 @@
 package example.JavaCollectionExample;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -28,12 +29,16 @@ import java.util.Map;
  *  - https://www.w3spoint.com/hashmap-linkedhashmap-treemap-hashtable-java
  *  - https://beginnersbook.com/2014/06/difference-between-hashmap-and-hashtable/
  *  - https://beginnersbook.com/java-collections-tutorials/
+ *  - https://www.geeksforgeeks.org/iterate-map-java/?ref=leftbar-rightbar
  */
 public class MapExample {
     public static void main(String[] args) {
         // example for HashMap.
         HashMapExample hme = new HashMapExample();
         hme.example();
+        hme.hashmapExample();
+        // hme.hashmapKeySetExample();
+        // hme.hashmapToIterator();
 
         // example for LinkedHashMap.
         LinkedHashMapExample lhme = new LinkedHashMapExample();
@@ -51,17 +56,18 @@ public class MapExample {
  *  - 每個元素皆有 key, value
  *  - 允許 null key, null value.
  * 
+ * Method:
+ *  - Map.entrySet()
+ *  - Map.Entry<K, V>
+ * 
  * Reference:
  *  - https://beginnersbook.com/2013/12/hashmap-in-java-with-example/
  * 
  */
 class HashMapExample {
     void example() {
-        HashMap<Integer, String> map = new HashMap<Integer, String>();
-
-        map.put(100, "george");
-        map.put(101, "may");
-        map.put(102, "JJ");
+        // * empty, remove, clear example for HashMap.
+        Map<Integer, String> map = generateData();
 
         System.out.println("content: " + map);
         System.out.println("# of element: " + map.size());
@@ -76,6 +82,62 @@ class HashMapExample {
 
         map.clear();
         System.out.println("content: " + map);
+    }
+
+    public Map<Integer, String> generateData() {
+        Map<Integer, String> map = new HashMap<Integer, String>();
+        map.put(100, "george");
+        map.put(101, "may");
+        map.put(102, "JJ");
+        return map;
+    }
+
+    public void hashmapExample() {
+        // * Map.entrySet() entries using for-each loop.
+        Map<Integer, String> map = generateData();
+
+        System.out.println("顯示hashmap資料: ");
+        System.out.println(map.entrySet());
+        
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            System.out.println("Key: " + entry.getKey() + ", " + "Value: " + entry.getValue());
+        }
+    }
+
+    public void hashmapKeySetExample() {
+        // * Map using keySet() and values() methods
+        Map<Integer, String> map = generateData();
+        
+        // using keySet() for iteration over keys.
+        for (Integer id : map.keySet()) {
+            System.out.println("key: " + id);
+        }
+
+        // using values() for iteration over values.
+        for (String name : map.values()) {
+            System.out.println("value: " + name);
+        }
+    }
+
+    public void hashmapToIterator() {
+        // * using iterators.
+        Map<Integer, String> map = generateData();
+
+        Iterator<Map.Entry<Integer, String>> itr = map.entrySet()
+            .iterator();
+        
+        while(itr.hasNext()) {
+            Map.Entry<Integer, String> entry = itr.next();
+            System.out.println("key: " + entry.getKey() + "; value: " + entry.getValue());
+        }
+    }
+
+    public void useForEachForHashMap() {
+        // * using foreach method.
+        Map<Integer, String> map = generateData();
+        map.forEach( (k, v) -> {
+            System.out.println("key: " + k + "; value: " + v);
+        });
     }
 }
 
