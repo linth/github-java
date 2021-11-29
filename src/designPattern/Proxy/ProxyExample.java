77e685b194fd58127f5ff731bb9a443fb5027d8a
linth <zhua0404@gmail.com>
2021-11-29 22:21:35 +0800

    add video class.

--- src/designPattern/Proxy/ProxyExample.java
+++ src/designPattern/Proxy/ProxyExample.java
@@ -1,5 +1,7 @@
 package designPattern.Proxy;
 
+import java.util.HashMap;
+
 /**
  * Proxy 範例
  * 
@@ -8,4 +10,22 @@ package designPattern.Proxy;
  */
 public class ProxyExample {
     
+}
+
+interface ThirdPartyTVLib {
+    HashMap<String, Video> popularVideos();
+
+    Video getVideo(String videoId);
+}
+
+class Video {
+    public String id;
+    public String title;
+    public String data;
+
+    Video(String id, String title) {
+        this.id = id;
+        this.title = title;
+        this.data = "Random video";
+    }
 }
