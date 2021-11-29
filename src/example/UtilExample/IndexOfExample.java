9a6ffc72e98f511dbf3ce7ae0dbfa54a1a7e5851
linth <zhua0404@gmail.com>
2021-11-29 22:20:54 +0800

    add comment.

--- src/example/UtilExample/IndexOfExample.java
+++ src/example/UtilExample/IndexOfExample.java
@@ -17,6 +17,7 @@ public class IndexOfExample {
     }
 
     public static void findString() {
+        // * find a specific string. 
         String string = "aaa456ac"; 
 
         System.out.println(string.indexOf("b")); // -1 不存在
@@ -25,6 +26,7 @@ public class IndexOfExample {
     }
 
     public static void findString2() {
+        // * find a specific string (method 2).
         String string = "a-97,b-98,c-99";
         System.out.println(string.indexOf(99)); // 10
         System.out.println(string.indexOf("c")); // 10
