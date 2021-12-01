d377727f88bc3f1327eb6ede04a0ad339d6e05ff
linth <zhua0404@gmail.com>
2021-12-01 23:49:57 +0800

    add Parallel Stream Example.

--- src/example/StreamExample/ParallelStreamExample.java
+++ src/example/StreamExample/ParallelStreamExample.java
@@ -1,9 +1,25 @@
 package example.StreamExample;
 
+import java.util.function.LongConsumer;
+import java.util.stream.LongStream;
+
 /**
  * Reference
  *  - https://mrbird.cc/Java8-parallelStream.html
  */
 public class ParallelStreamExample {
-    
+    public static void main(String[] args) {
+        ParallelStreamExample.test((n) -> LongStream.rangeClosed(1L, n).reduce(0L, Long::sum), 1000L);
+        ParallelStreamExample.test((n) -> LongStream.rangeClosed(1L, n).parallel().reduce(0L, Long::sum), 1000L);
+
+        ParallelStreamExample.test((n) -> LongStream.rangeClosed(1L, n).reduce(0L, Long::sum), 1000000000L);
+        ParallelStreamExample.test((n) -> LongStream.rangeClosed(1L, n).parallel().reduce(0L, Long::sum), 1000000000L);
+    }
+
+    static void test(LongConsumer c, Long n) {
+        long start = System.currentTimeMillis();
+        c.accept(n);
+        long end = System.currentTimeMillis();
+        System.out.println("處理時間" + (end - start) + "msc");
+    }
 }
