598afc1d0416cfbc71bc9e22d6bd31e9bb3f21db
linth <zhua0404@gmail.com>
2021-12-08 22:56:22 +0800

    add generic base example.

--- src/example/GenericExample/GenericBase.java
+++ src/example/GenericExample/GenericBase.java
@@ -1,5 +1,9 @@
 package example.GenericExample;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * Java泛型教學
  * 
@@ -7,5 +11,27 @@ package example.GenericExample;
  *  - https://tw511.com/20/209/8135.html
  */
 public class GenericBase {
-    
+    public static void main(String[] args) {
+        List<Integer> integerList = new ArrayList<Integer>();
+        integerList.add(Integer.valueOf(101));
+        integerList.add(Integer.valueOf(111));
+
+        List<String> stringList = new ArrayList<String>();
+        stringList.add("Java");
+        stringList.add("Python");
+        stringList.add("Javascript");
+
+        System.out.printf("Integer Value :%d\n", integerList.get(0));
+        System.out.printf("String Value :%s\n", stringList.get(0));
+
+        for (Integer data : integerList) {
+            System.out.printf("Integer Value :%d\n", data);
+        }
+
+        Iterator<String> stringIterator = stringList.iterator();
+
+        while (stringIterator.hasNext()) {
+            System.out.printf("String Value :%s\n", stringIterator.next());
+        }
+    }
 }
