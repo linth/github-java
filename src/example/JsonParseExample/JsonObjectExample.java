65efd26e6a062c12a7dcbd46dbe411d376d55178
linth <zhua0404@gmail.com>
2022-01-21 09:05:36 +0800

    add comments.

--- src/example/JsonParseExample/JsonObjectExample.java
+++ src/example/JsonParseExample/JsonObjectExample.java
@@ -1,7 +1,15 @@
 package example.JsonParseExample;
 
+/**
+ * ObjectMapper (JSON and Java Object)
+ * JSONObject
+ * 
+ * Reference:
+ *  - https://kucw.github.io/blog/2020/6/java-jackson/
+ *  - https://stackoverflow.com/questions/12155800/how-to-convert-hashmap-to-json-object-in-java
+ */
 public class JsonObjectExample {
     public static void main(String[] args) {
-
+        
     }
 }
