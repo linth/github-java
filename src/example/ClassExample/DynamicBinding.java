30ce1e0440807189aec0132fe62980366e37db49
linth <zhua0404@gmail.com>
2022-01-24 21:27:40 +0800

    add dynamic binding example.

---
+++ src/example/ClassExample/DynamicBinding.java
@@ -0,0 +1,15 @@
+package example.ClassExample;
+
+/**
+ * Java動態綁定機制經典案列理解
+ * 
+ * Reference:
+ *  - https://www.twblogs.net/a/5f054c8cb18dc407ccf04e96
+ */
+public class DynamicBinding {
+    public static void main(String[] args) {
+        
+    }
+
+    // TODO: add this example.
+}
