b9aab8e4feb8dbbb85610f9d8693b90332edb909
linth <zhua0404@gmail.com>
2022-02-21 22:16:58 +0800

    add comments for factory.

---
+++ src/designPattern/Factory/Factory.java
@@ -0,0 +1,12 @@
+package designPattern.Factory;
+
+/**
+ * Factory design pattern.
+ * 
+ * Reference:
+ *  - https://anexinet.com/blog/strategy-and-factory-patterns-in-a-spring-boot-application-part-i/
+ *  - https://azeynalli1990.medium.com/5-software-design-patterns-implemented-in-spring-88356dac738d
+ */
+public class Factory {
+    
+}
